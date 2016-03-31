24d8df340bb6d8c396baf27b06b6fa3cdd8d62fe
ALEX PARAMO <avp1983@list.ru>
2016-03-31 15:09:02 +0300

    parser

--- parser_2.py
+++ parser_2.py
@@ -10,7 +10,7 @@ from pydub.utils import mediainfo
 import os
 
 def log(msg):
-     f = open('parser.log', 'w')
+     f = open('parser.log', 'a')
      f.write(msg+"\n")
      f.close()
 
@@ -31,8 +31,8 @@ if soundLen < to:
 segment = sound[fr:to]'''         
 
 def findSilence(segment):    
-    minSilenceLen=500
-    silenceThresh=-50
+    minSilenceLen=100
+    silenceThresh=-60
     silenceSegmentsList = silence.detect_silence(segment, min_silence_len=minSilenceLen, silence_thresh=silenceThresh)
     if len(silenceSegmentsList)==0:
          log('not found silence segments with min_silence_len={0} and silence_thresh={1}'.format(minSilenceLen,silenceThresh))
@@ -52,15 +52,15 @@ def cutMp3(inFile,outDir):
      sound = AudioSegment.from_mp3(inFile)
      original_bitrate = mediainfo(inFile)['bit_rate']
      soundLen = len(sound)
-     fr = 6*1000
-     to = 15*1000    
+     minTime = 6*1000
+     maxTime = 15*1000    
      minSoundLen = 10*1000
      if soundLen < minSoundLen:
-         log("\t sound is less then {0} msec".format(minSoundLen))
+         log("   sound is less then {0} msec".format(minSoundLen))
          return 0
-     if soundLen < to:
-         log("\t sound  is less then {0} msec".format(to))
-         return [] 
+     if soundLen < maxTime:
+         log("   sound  is less then {0} msec".format(maxTime))
+         return 0 
      #cut = findSilence(sound[fr:to])
      #TempOutputFileName = trimExt(inFile)+'(1).mp3'
      #sound[0:cut].export(TempOutputFileName, format="mp3", bitrate=original_bitrate)
@@ -68,22 +68,41 @@ def cutMp3(inFile,outDir):
      count = 1
      begin = 0
      while timeLeft >= minSoundLen:
-         cut = findSilence(sound[begin+fr:begin+to])
-         soundToWrite = sound[begin:begin+cut]
-         soundLeft = sound[cut+1:soundLen]         
+         fr = begin+minTime
+         if fr>soundLen:
+             log("   Error: out of bounds,  from={0} msec".format(fr))
+             break;         
+         to = begin+maxTime
+         if to>soundLen:
+             to = soundLen
+         
+         silence = findSilence(sound[fr:to])
+         if not silence:
+             break
+         cut = begin + silence
+         soundToWrite = sound[begin:cut]
+         #soundLeft = sound[cut+1:soundLen]         
          TempOutputFileName = '{0}({1})_{2}_{3}.mp3'.format(getFileName(inFile),count,begin,cut)
          soundToWrite.export(os.path.join(outDir,TempOutputFileName), format="mp3", bitrate=original_bitrate)
+         log('    write file "{0}"'.format(TempOutputFileName))
          begin = cut+1
          count+=1
-         timeLeft = len(soundLeft) 
-        
+         timeLeft = soundLen-(cut+1) #len(soundLeft) 
+         
+     TempOutputFileName = '{0}({1})_{2}_{3}.mp3'.format(getFileName(inFile),count,cut+1,soundLen)
+     sound[cut+1:soundLen].export(os.path.join(outDir,TempOutputFileName), format="mp3", bitrate=original_bitrate)
+     log('    write file "{0}"'.format(TempOutputFileName))
+     return 1
+
+
+  
 
 def main():
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    inFile=r'Input_audio\0a94028e-c665-4c37-ac72-b40ae830aa97.mp3'
+    inFile=r'Input_audio\2af9a4f1-0071-47da-91ad-22c2a024e443.mp3'
     outDir=r'Output_audio'
    
     cutMp3(inFile, outDir)
-    print('ОК')
+    log("    ОК")
 
 main()    
