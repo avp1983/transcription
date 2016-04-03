fc9c9bdf241884655f183e15f4e3105232d7f4a4
ALEX PARAMO <avp1983@list.ru>
2016-04-03 15:43:26 +0300

    seek

--- transcription_joined.py
+++ transcription_joined.py
@@ -7,10 +7,11 @@ Created on Sat Apr  2 14:26:01 2016
 import os
 from pydub import AudioSegment,silence
 from pydub.utils import mediainfo
-
+import xml.sax
+import pandas as pd
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
+'''
 def  log(msg):
     f = open('compare.log', 'a')
     f.write(msg+"\n")
@@ -28,18 +29,13 @@ def compareSegments(seg1,seg2, step=200):
         to = (i+1)*step
         s1 = seg1[fr:to]
         s2 = seg2[fr:to]
-        #log('fr={0};to={1}'.format(fr,to))
         if s1.rms!=s2.rms:
             return False
     if  seg1[l*step:seg1Len].rms!=seg2[l*step:seg2Len].rms:
-        return False
-         
-    #for i,j in zip(seg1,seg2):
-    #    if i.rms!=j.rms: return False
-        
+        return False       
     return True             
 
-def findSegment(srcSeg, seekSeg, delta=0):
+def findSegment(srcSeg, seekSeg):
     lenSrcSeg = len(srcSeg)
     lenSeekSeg=len(seekSeg)
     
@@ -50,8 +46,6 @@ def findSegment(srcSeg, seekSeg, delta=0):
         audio_slice = srcSeg[i:i+lenSeekSeg]
         if compareSegments(audio_slice,seekSeg):
             return [i,i+lenSeekSeg]
-        #for s in audio_slice
-        #  delta_test=
     return []
     
     
@@ -78,5 +72,119 @@ def testFind2():
    
     r =  compareSegments(src, src)
     print(r)
+'''    
+#testFind() 
+class ParseHandler(xml.sax.ContentHandler):    
+    def __init__(self):
+        self.words=[]
+        
+    def startElement(self, name, attrs):
+        self._curentTag=name
+        if self._curentTag == "Word":
+            self._currentStime=float(attrs.get("stime"))*1000
+            self._currentDur=float(attrs.get("dur"))*1000
+        
+    def characters(self, content):    
+        if self._curentTag == "Word":
+             self.words.append({'word':content.rstrip(), 'stime':self._currentStime,'dur':self._currentDur})
     
-testFind()    
+
+class Cut():
+    def __init__(self,words):
+        self.words = words
+        self.currentMs=0
+        self.logFile='parser.log'
+        self.audioInDir = 'Input_audio'
+        self.txtOutDir = 'Output_text'
+        self.audioOutDir = 'Output_audio'
+        self.noiseOutDir = 'Output_noise'
+        self.fileNamesList=pd.read_csv('FileNames.csv')['Name'] 
+        self.pathToJoined='Joined.mp3'
+        self.joinedSound=[]
+        self.currentSound=[]
+        self.currentFile=''
+   
+    def  log(self,msg):
+        f = open(self.logFile, 'a')
+        f.write(msg+"\n")
+        f.close()    
+    
+    def trimExt(self, fileName=''):
+        return    os.path.splitext(fileName)[0]
+        
+    def run(self):
+        self.joinedSound = AudioSegment.from_mp3(self.pathToJoined)
+        for file in self.fileNamesList:
+            self.currentFile=self.trimExt(file)            
+            path = os.path.join(self.audioInDir, file)
+            self.processFile(path)
+       
+    
+    def compareSegments(self, seg1,seg2, step=200):
+        seg1Len = len(seg1)
+        seg2Len = len(seg2)
+        if seg1Len!=seg2Len:
+             raise Exception('invalid args')
+        l =  seg1Len//step
+        for i in range(0,l):
+            fr =  i*step;
+            to = (i+1)*step
+            s1 = seg1[fr:to]
+            s2 = seg2[fr:to]
+            if s1.rms!=s2.rms:
+                return False
+        if  seg1[l*step:seg1Len].rms!=seg2[l*step:seg2Len].rms:
+            return False       
+        return True             
+
+    def findSegment(self,srcSeg, seekSeg):
+        lenSrcSeg = len(srcSeg)
+        lenSeekSeg=len(seekSeg)
+        
+        if not srcSeg or not seekSeg or lenSeekSeg>lenSrcSeg:
+            raise Exception('invalid args')
+        seekTo = lenSrcSeg-lenSeekSeg+1
+        for i in range(seekTo):
+            audio_slice = srcSeg[i:i+lenSeekSeg]
+            if self.compareSegments(audio_slice,seekSeg):
+                return [i,i+lenSeekSeg]
+        return []    
+    
+    
+    def writeWords(self,fr,to):
+        text=''        
+        for word in self.words:
+            if word['stime']>=fr and (word['stime']+word['dur'])<to:
+                text+=word['word']
+        self.log('file={0}'.format(self.currentFile))
+        self.log(text)
+        self.log('--------------')        
+        
+    def  processFile(self, path):
+        self.currentSound = AudioSegment.from_mp3(path)
+        foundSeg = self.findSegment(self.joinedSound, self.currentSound)
+        if not foundSeg:
+            self.log('Sound from file {0} is not found in {1}'.format(path,self.pathToJoined))
+            return
+            
+        self.writeWords(foundSeg[0],foundSeg[1])    
+            
+        
+        
+        
+       
+
+def main():
+    parser = xml.sax.make_parser()
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+    handler = ParseHandler()
+    parser.setContentHandler(handler)
+    parser.parse("full.xml")
+    words = handler.words
+    
+    c = Cut(words)
+    c.run()
+    
+   
+    
+main()    
